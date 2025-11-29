ff42482dea941dbd7f22776070be62fc04e804e1
Javinder2004 <jsvndrsnghjss@gmail.com>
2025-11-29 10:32:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Consecutive Characters/remove-consecutive-characters.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    public String removeConsecutiveCharacter(String s) {
+        // code here
+        char c[]=new char[s.length()];
+        int j=0;
+        c[0]=s.charAt(0);
+        j=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)!=c[j-1]){
+            c[j]=s.charAt(i);
+            j++;
+        }
+            
+        }
+     
+          String f=String.valueOf(c,0,j);
+
+return f;
+    }
+}
